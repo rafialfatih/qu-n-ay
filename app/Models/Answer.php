9ad6801048da6389032fb592aaaf840aedc6ef69
rafi <rafi.alfatih@gmail.com>
2022-09-14 19:34:47 +0700

    Add new model

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Answer extends Model
 {
@@ -15,17 +17,17 @@ class Answer extends Model
         'answer'
     ];
 
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
 
-    public function question()
+    public function question(): BelongsTo
     {
         return $this->belongsTo(Question::class);
     }
 
-    public function votes()
+    public function votes(): HasMany
     {
         return $this->hasMany(AnswerVote::class);
     }
--- app/Models/QuestionVote.php
+++ app/Models/QuestionVote.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class QuestionVote extends Model
 {
@@ -15,12 +16,12 @@ class QuestionVote extends Model
         'vote'
     ];
 
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
 
-    public function question()
+    public function question(): BelongsTo
     {
         return $this->belongsTo(Question::class);
     }
