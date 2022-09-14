0f34b5afbbd203ed943c4a97e27f4314972f48b0
rafi <rafi.alfatih@gmail.com>
2022-09-14 19:31:46 +0700

    Add scope for votes

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -4,15 +4,39 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\DB;
 
 class Question extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['user_id', 'title', 'question', 'tags'];
+    protected $fillable = [
+        'user_id',
+        'title',
+        'slug',
+        'question',
+        'tags'
+    ];
 
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function answers(): HasMany
+    {
+        return $this->hasMany(Answer::class);
+    }
+
+    public function votes(): HasMany
+    {
+        return $this->hasMany(QuestionVote::class);
+    }
+
+    public function scopeVotes($query)
+    {
+        return $query->withCount(['votes as upvotes_count' => fn ($query) => $query->where('vote', 'up')]);
     }
 }
