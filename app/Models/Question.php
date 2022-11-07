326108d4717468f36c13327c364821bea1e531aa
rafi <rafi.alfatih@gmail.com>
2022-11-07 16:23:14 +0700

    Change id to uuid

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -5,19 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Support\Facades\DB;
+use App\Http\Traits\UseUuid;
 
 class Question extends Model
 {
     use HasFactory;
+    use UseUuid;
 
     protected $fillable = [
         'user_id',
         'title',
         'slug',
         'question',
-        'tags'
     ];
 
     public function user(): BelongsTo
@@ -33,6 +34,11 @@ class Question extends Model
     public function votes(): HasMany
     {
         return $this->hasMany(QuestionVote::class);
+    }
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
     }
 
     public function scopeVotes($query)
