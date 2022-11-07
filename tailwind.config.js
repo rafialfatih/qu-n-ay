1453efca13881239bddaf585aaf1b03691dbde04
rafi <rafi.alfatih@gmail.com>
2022-11-07 16:28:13 +0700

    Add base color

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
         "./resources/**/*.vue",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                base: "#edf0fc",
+            },
+        },
     },
     plugins: [],
 };
