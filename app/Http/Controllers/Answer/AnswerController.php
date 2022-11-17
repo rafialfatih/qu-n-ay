4dfa63c7742374ff24665534111496ca8131112d
rafi <rafi.alfatih@gmail.com>
2022-11-17 21:01:29 +0700

    Add answer controller

---
+++ app/Http/Controllers/Answer/AnswerController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Answer;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Answer\StoreAnswerRequest;
+use App\Http\Requests\Answer\UpdateAnswerRequest;
+use App\Models\Answer;
+
+class AnswerController extends Controller
+{
+    public function store(StoreAnswerRequest $request)
+    {
+        $answer = $request->safe()->merge([
+            'user_id' => auth()->id(),
+            'question_id' => $request->question_id
+        ]);
+
+        Answer::create($answer->all());
+
+        return back()->with('message', 'Your answer has been submitted');
+    }
+
+    public function update(UpdateAnswerRequest $request, Answer $answer)
+    {
+        $update = $request->validated();
+    }
+}
