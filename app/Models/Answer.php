95255636d1bd9d407b343379fbf81097f2d62d37
rafi <rafi.alfatih@gmail.com>
2022-11-17 21:04:14 +0700

    Change ID to UUID

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -6,10 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Http\Traits\UseUuid;
 
 class Answer extends Model
 {
     use HasFactory;
+    use UseUuid;
 
     protected $fillable = [
         'user_id',
