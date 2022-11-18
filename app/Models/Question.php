a09b899f4404c6d844b1d3dc9783a7620e8e0639
rafi <rafi.alfatih@gmail.com>
2022-11-18 20:37:28 +0700

    Add get attribute for created_at

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -7,45 +7,64 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use App\Http\Traits\UseUuid;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Builder;
+use App\Http\Traits\UseUuid;
+use Carbon\Carbon;
 
 class Question extends Model
 {
-    use HasFactory;
-    use UseUuid;
+  use HasFactory;
+  use UseUuid;
 
-    protected $fillable = [
-        'user_id',
-        'title',
-        'slug',
-        'question',
-    ];
+  protected $dates = [
+    'created_at',
+    'updated_at'
+  ];
 
-    public function user(): BelongsTo
-    {
-        return $this->belongsTo(User::class);
-    }
+  protected $fillable = [
+    'user_id',
+    'title',
+    'slug',
+    'question',
+  ];
 
-    public function answers(): HasMany
-    {
-        return $this->hasMany(Answer::class);
-    }
+  public function user(): BelongsTo
+  {
+    return $this->belongsTo(User::class);
+  }
 
-    public function votes(): HasMany
-    {
-        return $this->hasMany(QuestionVote::class);
-    }
+  public function answers(): HasMany
+  {
+    return $this->hasMany(Answer::class);
+  }
 
-    public function tags(): BelongsToMany
-    {
-        return $this->belongsToMany(Tag::class);
-    }
+  public function votes(): HasMany
+  {
+    return $this->hasMany(QuestionVote::class);
+  }
 
-    public function scopeVotes($query)
-    {
-        $query->withCount(
-            ['votes as upvotes_count' => fn (Builder $query) => $query->where('vote', 'up')]
-        );
-    }
+  public function tags(): BelongsToMany
+  {
+    return $this->belongsToMany(Tag::class);
+  }
+
+  public function scopeVotes($query)
+  {
+    $query->withCount(
+      ['votes as upvotes_count' => fn (Builder $query) => $query->where('vote', 'up')]
+    );
+  }
+
+  public function scopeAnswers($query)
+  {
+    $query->withCount('answers');
+  }
+
+  public function createdAt(): Attribute
+  {
+    return Attribute::make(
+      get: fn ($value) => Carbon::parse($value)->shortRelativeDiffForHumans()
+    );
+  }
 }
