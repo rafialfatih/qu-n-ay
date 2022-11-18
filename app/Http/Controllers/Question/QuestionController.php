5a3a4bfa50e187336b50e9e159a8914a3b4f2346
rafi <rafi.alfatih@gmail.com>
2022-11-18 20:37:01 +0700

    Add get attribute for created_at

--- app/Http/Controllers/Question/QuestionController.php
+++ app/Http/Controllers/Question/QuestionController.php
@@ -7,140 +7,139 @@ use App\Models\Question;
 use App\Http\Requests\Question\StoreQuestionRequest;
 use App\Http\Requests\Question\UpdateQuestionRequest;
 use App\Services\QuestionService;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Str;
 
 class QuestionController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        return view('questions.index', [
-            'questions' => Question::with(['user', 'tags'])
-                ->votes()
-                ->orderBy('created_at', 'desc')
-                ->get()
-        ]);
-    }
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    return view('questions.index', [
+      'questions' => Question::with(['user', 'tags'])
+        ->votes()
+        ->answers()
+        ->orderBy('created_at', 'desc')
+        ->paginate(15)
+    ]);
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        return view('questions.create');
-    }
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function create()
+  {
+    return view('questions.create');
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreQuestionRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(StoreQuestionRequest $request, QuestionService $questionService)
-    {
-        $store = $request->safe()
-            ->merge([
-                'user_id' => auth()->id(),
-                'slug' => Str::slug($request->title)
-            ]);
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \App\Http\Requests\StoreQuestionRequest  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(StoreQuestionRequest $request, QuestionService $questionService)
+  {
+    $store = $request->safe()
+      ->merge([
+        'user_id' => auth()->id(),
+        'slug' => Str::slug($request->title)
+      ]);
 
-        $questionService->createQuestion($store->all());
+    $questionService->createQuestion($store->all());
 
-        return redirect('/questions')->with('message', 'Your question has been submitted');
-    }
+    return redirect('/questions')->with('message', 'Your question has been submitted');
+  }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Question  $question
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Question $question, $slug)
-    {
-        abort_if(
-            $slug !== $question->slug,
-            404,
-        );
+  /**
+   * Display the specified resource.
+   *
+   * @param  \App\Models\Question  $question
+   * @return \Illuminate\Http\Response
+   */
+  public function show(Question $question, $slug)
+  {
+    abort_if(
+      $slug !== $question->slug,
+      404,
+    );
 
-        $question = Question::with(['user', 'tags', 'answers'])
-            ->votes()
-            ->where('id', $question->id)
-            ->firstOrFail();
+    $question = Question::with(['user', 'tags', 'answers'])
+      ->votes()
+      ->where('id', $question->id)
+      ->firstOrFail();
 
-        return view('questions.show', [
-            'question' => $question,
-        ]);
-    }
+    return view('questions.show', [
+      'question' => $question,
+    ]);
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Question  $question
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Question $question, QuestionService $questionService, $slug)
-    {
-        abort_if(
-            $question->user_id !== auth()->id(),
-            403,
-        );
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  \App\Models\Question  $question
+   * @return \Illuminate\Http\Response
+   */
+  public function edit(Question $question, QuestionService $questionService, $slug)
+  {
+    abort_if(
+      $question->user_id !== auth()->id(),
+      403,
+    );
 
-        abort_if(
-            $question->slug !== $slug,
-            404,
-        );
+    abort_if(
+      $question->slug !== $slug,
+      404,
+    );
 
-        $question = Question::where('id', $question->id)->firstOrFail();
-        $tags = $questionService->editQuestionTag($question->tags);
+    $question = Question::where('id', $question->id)->firstOrFail();
+    $tags = $questionService->editQuestionTag($question->tags);
 
-        return view('questions.edit', [
-            'question' => $question,
-            'tags' => $tags
-        ]);
-    }
+    return view('questions.edit', [
+      'question' => $question,
+      'tags' => $tags
+    ]);
+  }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateQuestionRequest  $request
-     * @param  \App\Models\Question  $question
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UpdateQuestionRequest $request, Question $question, QuestionService $questionService)
-    {
-        $update = $request->safe()->merge([
-            'slug' => Str::slug($request->title)
-        ]);
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \App\Http\Requests\UpdateQuestionRequest  $request
+   * @param  \App\Models\Question  $question
+   * @return \Illuminate\Http\Response
+   */
+  public function update(UpdateQuestionRequest $request, Question $question, QuestionService $questionService)
+  {
+    $update = $request->safe()->merge([
+      'slug' => Str::slug($request->title)
+    ]);
 
-        $questionService->updateQuestion($update->all(), $question->id);
+    $questionService->updateQuestion($update->all(), $question->id);
 
-        return redirect()
-            ->route('question.show', ['question' => $question, 'slug' => $update->slug])
-            ->with('message', 'Your question has been updated!');
-    }
+    return redirect()
+      ->route('question.show', ['question' => $question, 'slug' => $update->slug])
+      ->with('message', 'Your question has been updated!');
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Question  $question
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Question $question)
-    {
-        abort_if(
-            $question->user_id !== auth()->id(),
-            403,
-        );
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  \App\Models\Question  $question
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy(Question $question)
+  {
+    abort_if(
+      $question->user_id !== auth()->id(),
+      403,
+    );
 
-        $question->tags->detach();
-        $question->delete();
-    }
+    $question->tags->detach();
+    $question->delete();
+  }
 }
--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -7,30 +7,39 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use App\Http\Traits\UseUuid;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Answer extends Model
 {
-    use HasFactory;
-    use UseUuid;
+  use HasFactory;
+  use UseUuid;
 
-    protected $fillable = [
-        'user_id',
-        'question_id',
-        'answer'
-    ];
+  protected $fillable = [
+    'user_id',
+    'question_id',
+    'answer'
+  ];
 
-    public function user(): BelongsTo
-    {
-        return $this->belongsTo(User::class);
-    }
+  public function user(): BelongsTo
+  {
+    return $this->belongsTo(User::class);
+  }
 
-    public function question(): BelongsTo
-    {
-        return $this->belongsTo(Question::class);
-    }
+  public function question(): BelongsTo
+  {
+    return $this->belongsTo(Question::class);
+  }
 
-    public function votes(): HasMany
-    {
-        return $this->hasMany(AnswerVote::class);
-    }
+  public function votes(): HasMany
+  {
+    return $this->hasMany(AnswerVote::class);
+  }
+
+  public function createdAt(): Attribute
+  {
+    return Attribute::make(
+      get: fn ($value) => Carbon::parse($value)->shortRelativeDiffForHumans()
+    );
+  }
 }
