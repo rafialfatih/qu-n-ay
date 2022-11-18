9aeffcc4e54756e36431c3de7b473a4306148602
rafi <rafi.alfatih@gmail.com>
2022-11-18 20:22:24 +0700

    Add store answer

--- app/Http/Controllers/Answer/AnswerController.php
+++ app/Http/Controllers/Answer/AnswerController.php
@@ -9,20 +9,28 @@ use App\Models\Answer;
 
 class AnswerController extends Controller
 {
-    public function store(StoreAnswerRequest $request)
-    {
-        $answer = $request->safe()->merge([
-            'user_id' => auth()->id(),
-            'question_id' => $request->question_id
-        ]);
+  public function store(StoreAnswerRequest $request)
+  {
+    $answer = $request->safe()->merge([
+      'user_id' => auth()->id(),
+      'question_id' => $request->question_id
+    ]);
 
-        Answer::create($answer->all());
+    Answer::create($answer->all());
 
-        return back()->with('message', 'Your answer has been submitted');
-    }
+    return back()->with('message', 'Your answer has been submitted');
+  }
 
-    public function update(UpdateAnswerRequest $request, Answer $answer)
-    {
-        $update = $request->validated();
-    }
+  public function update(UpdateAnswerRequest $request, Answer $answer)
+  {
+    $update = $request->validated();
+  }
+
+  public function destroy(Answer $answer)
+  {
+    abort_if(
+      $answer->user_id !== auth()->id(),
+      403,
+    );
+  }
 }
