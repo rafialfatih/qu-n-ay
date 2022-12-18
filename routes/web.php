1b7eddc309daab3eabd23ce938395450e9862adf
rafi <rafi.alfatih@gmail.com>
2022-12-18 21:37:41 +0700

    Refactor code

--- routes/web.php
+++ routes/web.php
@@ -25,55 +25,61 @@ Route::get('/', function () {
 });
 
 Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisterUserController::class, 'create'])
-      ->name('auth.register');
-    Route::post('register', [RegisterUserController::class, 'store'])
-      ->name('auth.store');
+    Route::name('auth.')->group(function () {
+        Route::get('register', [RegisterUserController::class, 'create'])
+            ->name('register');
+        Route::post('register', [RegisterUserController::class, 'store'])
+            ->name('store');
 
-    Route::get('login', [AuthenticateUserController::class, 'create'])
-      ->name('auth.create');
+        Route::get('login', [AuthenticateUserController::class, 'create'])
+            ->name('create');
+    });
 });
 
 Route::middleware('auth')->group(function () {
     Route::resource('questions', QuestionController::class)
-      ->except(['show', 'index', 'edit'])
-      ->names([
-          'create' => 'question.create',
-          'store' => 'question.store',
-          'update' => 'question.update',
-          'destroy' => 'question.destroy',
-      ]);
+        ->except(['show', 'index', 'edit'])
+        ->names([
+            'create' => 'question.create',
+            'store' => 'question.store',
+            'update' => 'question.update',
+            'destroy' => 'question.destroy',
+        ]);
     Route::get('questions/{question}/{slug?}/edit', [QuestionController::class, 'edit'])
-      ->name('question.edit');
+        ->name('question.edit');
 
-    Route::post('answers', [AnswerController::class, 'store'])
-      ->name('answer.store');
-    Route::put('answers/{answer}', [AnswerController::class, 'update'])
-      ->name('answer.update');
-    Route::get('questions/{question}/answers/{answer}/edit', [AnswerController::class, 'edit'])
-      ->name('answer.edit');
-    Route::put('questions/{question}/answers/{answer}', [AnswerController::class, 'update'])
-      ->name('answer.update');
-    Route::delete('questions/{question}/answers/{answer}', [AnswerController::class, 'destroy'])
-      ->name('answer.delete');
+    Route::name('answer.')->group(function () {
+        Route::post('answers', [AnswerController::class, 'store'])
+            ->name('store');
+        Route::put('answers/{answer}', [AnswerController::class, 'update'])
+            ->name('update');
+        Route::get('questions/{question}/answers/{answer}/edit', [AnswerController::class, 'edit'])
+            ->name('edit');
+        Route::put('questions/{question}/answers/{answer}', [AnswerController::class, 'update'])
+            ->name('update');
+        Route::delete('questions/{question}/answers/{answer}', [AnswerController::class, 'destroy'])
+            ->name('delete');
+    });
 
     Route::post('question-vote', QuestionVoteController::class)
-      ->name('question_vote');
+        ->name('question_vote');
 
     Route::delete('logout', [AuthenticateUserController::class, 'destroy'])
-      ->name('auth.logout');
+        ->name('auth.logout');
 });
 
-Route::get('questions', [QuestionController::class, 'index'])
-  ->name('question.index');
-Route::get('questions/{question}/{slug?}', [QuestionController::class, 'show'])
-  ->name('question.show');
+Route::name('question.')->group(function () {
+    Route::get('questions', [QuestionController::class, 'index'])
+        ->name('index');
+    Route::get('questions/{question}/{slug?}', [QuestionController::class, 'show'])
+        ->name('show');
 
-Route::get('search', QuestionSearchController::class)
-  ->name('question.search');
+    Route::get('search', QuestionSearchController::class)
+        ->name('search');
+});
 
 Route::get('user/{user:username}/', [UserController::class, 'show'])
-  ->name('user.show');
+    ->name('user.show');
 
 Route::post('login', [AuthenticateUserController::class, 'store'])
-  ->name('auth.login');
+    ->name('auth.login');
