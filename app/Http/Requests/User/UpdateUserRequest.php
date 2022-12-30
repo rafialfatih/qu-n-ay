dc3a877e3a2c4548c8b969798841da68c41c61a9
rafi <rafi.alfatih@gmail.com>
2022-12-30 14:35:23 +0700

    Add UpdateUserRequest request

---
+++ app/Http/Requests/User/UpdateUserRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => [
+                'required',
+                'min:3',
+                'max:30',
+                'string'
+            ],
+            'username' => [
+                'required',
+                'min:6',
+                'max:20',
+                'regex:/^[a-zA-Z0-9][\w\.]+[a-zA-Z0-9]$/',
+                Rule::unique('users', 'username')->ignore($this->user)
+            ],
+            'email' => [
+                'required',
+                'email',
+                'max:50',
+                Rule::unique('users', 'email')->ignore($this->user)
+            ],
+        ];
+    }
+}
