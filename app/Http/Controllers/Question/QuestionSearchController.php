dd2b413c815298ac9b3850fbd62061f6613af787
rafi <rafi.alfatih@gmail.com>
2023-01-02 20:55:56 +0700

    Remove request and refactor code

--- app/Http/Controllers/Question/QuestionSearchController.php
+++ app/Http/Controllers/Question/QuestionSearchController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Question;
 
 use App\Http\Controllers\Controller;
 use App\Models\Question;
-use Illuminate\Http\Request;
 
 class QuestionSearchController extends Controller
 {
@@ -14,17 +13,17 @@ class QuestionSearchController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function __invoke()
     {
         return view('search.index', [
-            'search' => Question::with(['tags', 'user'])
+            'search' => Question::with(['user'])
                 ->join('question_tag', 'questions.id', '=', 'question_tag.question_id')
                 ->join('tags', 'tags.id', '=', 'question_tag.tag_id')
                 ->select('questions.*')
                 ->filter(request(['tag', 'q']))
                 ->groupBy('questions.title')
                 ->orderByDesc('created_at')
-                ->paginate(15)
+                ->paginate(15),
         ]);
     }
 }
